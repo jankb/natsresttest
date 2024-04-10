202863569e328bbead3c57d47061c5799994ae38
Jan Kristian Bueie <jan.kristian.bueie@mattilsynet.no>
2024-04-10 10:02:56 +0200

    Added rest + nats example

---
+++ restnatsserver/src/main/kotlin/com/example/restnatsserver/RestnatsserverApplication.kt
@@ -0,0 +1,11 @@
+package com.example.restnatsserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RestnatsserverApplication
+
+fun main(args: Array<String>) {
+	runApplication<RestnatsserverApplication>(*args)
+}
---
+++ restnatsserver/src/test/kotlin/com/example/restnatsserver/RestnatsserverApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.restnatsserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RestnatsserverApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
