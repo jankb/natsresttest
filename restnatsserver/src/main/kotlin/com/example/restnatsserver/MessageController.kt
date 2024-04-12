896ec52781247ba0805ae6e79c44626ee500274f
Jan Kristian Bueie <jan.kristian.bueie@mattilsynet.no>
2024-04-12 14:30:14 +0200

    fixed imports

--- restnatsserver/src/main/kotlin/com/example/restnatsserver/MessageController.kt
+++ restnatsserver/src/main/kotlin/com/example/restnatsserver/MessageController.kt
@@ -1,17 +1,25 @@
 package com.example.restnatsserver
 
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/message")
 class MessageController(private val messageService: MessageService) {
-
     @PostMapping
-    fun postMessage(@RequestBody message: Map<String, String>): Message {
+    fun postMessage(
+        @RequestBody message: Map<String, String>,
+    ): Message {
         val msg = message["message"] ?: throw IllegalArgumentException("Message is required")
         return messageService.saveMessage(msg)
     }
 
     @GetMapping("/{id}")
-    fun getMessage(@PathVariable id: Long): Message? = messageService.getMessage(id)
+    fun getMessage(
+        @PathVariable id: Long,
+    ): Message? = messageService.getMessage(id)
 }
