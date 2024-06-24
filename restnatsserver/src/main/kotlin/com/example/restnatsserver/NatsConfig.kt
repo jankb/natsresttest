edffea5c5d5eb56c6962428b59b23b4919e5af7b
Jan Kristian Bueie <jan.kristian.bueie@mattilsynet.no>
2024-06-24 21:32:35 +0200

    Updated config.

--- restnatsserver/src/main/kotlin/com/example/restnatsserver/NatsConfig.kt
+++ restnatsserver/src/main/kotlin/com/example/restnatsserver/NatsConfig.kt
@@ -2,6 +2,7 @@ package com.example.restnatsserver
 
 import io.nats.client.Connection
 import io.nats.client.Nats
+import io.nats.client.Options
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -9,5 +10,6 @@ import org.springframework.context.annotation.Configuration
 class NatsConfig {
 
     @Bean
-    fun natsConnection(): Connection = Nats.connect("nats://localhost:4222")
+    //fun natsConnection(): Connection = Nats.connect("nats://localhost:4222")
+    fun natsConnection(): Connection = Nats.connect(Options.builder().server("nats://localhost:4222").userInfo("sys", "pass").build())
 }
